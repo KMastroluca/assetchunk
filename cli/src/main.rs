a9ce92f645362bfcf4a273745e4cee2b9dc258c3
Lorenzo A. Banks <kasene.mastroluca-clark.@maine.edu>
2023-11-25 23:48:54 -0500

    Added some pretty nice menu options to the CLI.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,107 +3,10 @@ mod pack;
 
 mod term;
 
-// Asset Chunk Library
-use assetchunk::AssetManifest;
-use pack::print_manifest_contents;
-
-use std::io::Error;
-use std::path::PathBuf;
-
-use clap::Parser;
-use clap::Subcommand;
-use clap::ValueHint;
-
-// Some Crossterm Shit
-use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::execute;
-use std::io::{stdout, Write};
-
-
-
-
-
-#[derive(Debug, Parser)]
-#[command(name = "assetchunk")]
-#[command(about = "Asset Chunking Tool For The JRPGine")]
-struct CLI {
-    #[command(subcommand)]
-    command: Commands
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-
-    #[command(arg_required_else_help = true)]
-    // Pack Assets Into A Chunk
-    Pack {
-        #[clap(value_hint = ValueHint::DirPath)]
-        input_dir: Option<PathBuf>,
-        #[clap(short = 'o', long = "output", value_hint = ValueHint::FilePath)]
-        output_path: Option<PathBuf>,
-    },
-
-    #[command(arg_required_else_help = true)]
-    List {
-        #[clap(value_hint = ValueHint::FilePath)]
-        asset_manifest_load_path: Option<PathBuf>,
-    },
-
-    #[command(arg_required_else_help = true)]
-    Test {
-        #[clap(short = 'l', long = "load", value_hint = ValueHint::FilePath)]
-        asset_file_load_path: Option<PathBuf>,
-    }
-
-
-
-    // Unpack Assets From A Chunk
-
-
-}
 
 fn main() -> std::io::Result<()> {
 
-    let args = CLI::parse();
-
     let mut stdout = std::io::stdout();
 
-    term::interactive_term(&mut stdout);
-
-    match args.command {
-        Commands::List {asset_manifest_load_path} => {
-            if asset_manifest_load_path.is_none() {
-                
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    format!("[-] No Asset Manifest File Specified.")
-                ));
-
-            } else {
-                print_manifest_contents(asset_manifest_load_path.unwrap());
-                return Ok(());
-            }
-        },
-        Commands::Pack { input_dir, output_path } => {
-            pack::pack(input_dir, output_path);
-            return Ok(());
-        },
-        Commands::Test { asset_file_load_path } => {
-            let filedata = std::fs::read(asset_file_load_path.unwrap());
-            if filedata.is_ok() {
-                let filedata = filedata.unwrap();
-                let datastr = String::from_utf8(filedata);
-                if datastr.is_ok() {
-                    let datastr = datastr.unwrap();
-                    let mut manifest:AssetManifest = AssetManifest::from_json(&datastr);
-                    println!("{:?}", manifest);
-                    manifest.load_asset_data("./out.chunk.asset");
-                }
-                return Ok(());
-            } else {
-                return Err(Error::new(std::io::ErrorKind::InvalidInput, "[-] Failed To Load Asset File From Disk."));
-            }
-            
-        }
-    }
+    term::interactive_term(&mut stdout)
 }
--- cli/src/term.rs
+++ cli/src/term.rs
@@ -2,12 +2,14 @@ use std::io;
 use crossterm::cursor;
 use crossterm::queue;
 use crossterm::style;
+use crossterm::style::Stylize;
 use crossterm::terminal;
 use crossterm::execute;
 use crossterm::terminal::ClearType;
 use crossterm::terminal::EnterAlternateScreen;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, EventStream, KeyEventKind};
 
+use std::io::{stdout, Write};
 
 pub enum MenuState {
    Main,
@@ -52,7 +54,6 @@ const MENU: &str = r#"
 "#;
 
 
-
 pub fn interactive_term<W>(w: &mut W) -> std::io::Result<()>
 where 
    W: io::Write, {
@@ -71,7 +72,10 @@ where
             }
          },
          Some(MenuState::Pack) => {
-            println!("Pack");
+            if draw_pack_menu(w, &mut screen_stack).is_err() {
+               eprintln!("[-] Error drawing pack menu");
+               break;
+            }
          },
          Some(MenuState::Unpack) => {
             println!("Unpack");
@@ -117,6 +121,198 @@ pub fn buffer_size() -> std::io::Result<(u16, u16)> {
    terminal::size()
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub enum InputMode {
+   Nav,
+   Input(InputField)
+}
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub enum InputField {
+   InputDir,
+   OutputName
+}
+
+pub fn draw_pack_menu<W>(w:&mut W, screen_stack:&mut ScreenStack) -> std::io::Result<()>
+where 
+   W: io::Write, {
+      
+
+      let mut input_mode = InputMode::Nav;
+   
+      let mut selection = 0;
+      let mut cursor_pos = (0, 0);
+      let mut input_dir = String::new();
+      let mut input_dir_editing = String::new();
+      let mut output_name = String::new();
+      let mut output_name_editing = String::new();
+
+      loop {
+
+         queue!(w, 
+            terminal::Clear(ClearType::All),
+            cursor::Hide,
+            cursor::MoveTo(0, 0),
+         ).unwrap();
+
+         // So That We Have An Indicator Of What We Are Editing
+         if selection == 1 && input_mode == InputMode::Input(InputField::InputDir) {
+            input_dir_editing = " [EDITING]".to_string();
+            output_name_editing = "".to_string();
+         } else if selection == 2 && input_mode == InputMode::Input(InputField::OutputName){
+            output_name_editing = " [EDITING]".to_string();
+            input_dir_editing = "".to_string();
+         } else {
+            input_dir_editing = "".to_string();
+            output_name_editing = "".to_string();
+         }
+
+         match selection {
+            1 => {
+               queue!(w, style::PrintStyledContent(format!("=> Input Directory: {} {}", input_dir, input_dir_editing).blue()), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Output Name: {}", output_name)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print("Pack Assets"), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::PrintStyledContent("Go Back".red()), cursor::MoveToNextLine(1)).unwrap();
+            },
+            2 => {
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Input Directory: {}", input_dir)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, style::PrintStyledContent(format!("=> Output Name: {} {}", output_name, output_name_editing).blue()), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print("Pack Assets"), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::PrintStyledContent("Go Back".red()), cursor::MoveToNextLine(1)).unwrap();
+            },
+            3 => {
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Input Directory: {}", input_dir)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Output Name: {}", output_name)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, style::PrintStyledContent("=> Pack Assets".blue()), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::PrintStyledContent("Go Back".red()), cursor::MoveToNextLine(1)).unwrap();
+            }
+            0 => {
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Input Directory: {}", input_dir)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print(format!("Output Name: {}", output_name)), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, cursor::MoveRight(3), style::Print("Pack Assets"), cursor::MoveToNextLine(1)).unwrap();
+               queue!(w, style::PrintStyledContent("=> Go Back".red()), cursor::MoveToNextLine(1)).unwrap();
+            }
+            _ =>{}
+         }
+
+         queue!(w, cursor::MoveTo(0, buffer_size().unwrap().1 - 6), style::Print("__ DETAILS ___________________________________________")).unwrap();
+
+         // Show Asset File Name & Manifest File Name + Path
+         if output_name.len() > 0 {
+            queue!(w, cursor::MoveTo(3, buffer_size().unwrap().1 - 5), style::PrintStyledContent(format!("Asset Chunk File: {}.chunk.asset", output_name).magenta())).unwrap();
+            queue!(w, cursor::MoveTo(3, buffer_size().unwrap().1 - 4), style::PrintStyledContent(format!("Asset Manifest File: {}.manifest.asset", output_name).magenta())).unwrap();
+         }
+
+
+
+
+
+         w.flush().unwrap();
+
+         if let Ok(Event::Key(event)) = event::read() {
+
+            match event.kind {
+
+               KeyEventKind::Release => {
+
+                  if input_mode == InputMode::Nav {
+
+                     match event.code {
+
+                        KeyCode::Up => {
+                           // Move up
+                           if selection == 0 {
+                              selection = 3;
+                           } else {
+                              selection -= 1;
+                           }
+                        },
+
+                        KeyCode::Down => {
+                           // Move down
+                           if selection == 3 {
+                              selection = 0;
+                           } else {
+                              selection += 1;
+                           }
+                        },
+
+                        KeyCode::Enter => {
+                           // Enter
+                           match selection {
+                              0 => {
+                                 // Go Back
+                                 screen_stack.pop();
+                              },
+                              1 => {
+                                 // Input Dir
+                                 input_mode = InputMode::Input(InputField::InputDir);
+                              },
+                              2 => {
+                                 // Output Name
+                                 input_mode = InputMode::Input(InputField::OutputName);
+                              },
+                              3 => {
+                                 // Pack Assets
+                                 println!("Pack Assets");
+                              },
+                              _ => {}
+                           }
+                        },
+
+                        _ => {}
+
+                     }
+
+                  } else if  input_mode == InputMode::Input(InputField::InputDir) {
+
+                     match event.code {
+
+                        KeyCode::Esc => {
+                           input_mode = InputMode::Nav;
+                        },
+
+                        KeyCode::Backspace => {
+                           input_dir.pop();
+                        },
+
+                        KeyCode::Char(c) => {
+                           input_dir.push(c);
+                        },
+
+                        _ => {}
+
+                     }
+
+                  } else if input_mode == InputMode::Input(InputField::OutputName) {
+
+                     match event.code {
+
+                        KeyCode::Esc => {
+                           input_mode = InputMode::Nav;
+                        },
+
+                        KeyCode::Backspace => {
+                           output_name.pop();
+                        },
+
+                        KeyCode::Char(c) => {
+                           output_name.push(c);
+                        },
+
+                        _ => {}
+
+                     }
+                      
+                  }
+               },
+               _ => {}
+            }
+         }
+
+
+      }
+}
 
 pub fn draw_main_menu<W>(w: &mut W, screen_stack:&mut ScreenStack) -> std::io::Result<()>
 where 
@@ -128,8 +324,8 @@ where
       cursor::Hide,
       cursor::MoveTo(1, 1),).unwrap();
 
-      for LINE in MENU.split("\n") {
-         queue!(w, style::Print(LINE), cursor::MoveToNextLine(1)).unwrap();
+      for line in MENU.split("\n") {
+         queue!(w, style::Print(line), cursor::MoveToNextLine(1)).unwrap();
       }
 
       w.flush().unwrap();
@@ -161,3 +357,4 @@ where
 
    Ok(())
 }
+   
